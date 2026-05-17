2b5f76d936aa3c76a98076aa6449375e0caad087
martach9 <martach@usal.es>
2026-05-17 19:21:42 +0200

    Update logout.pl

--- ficheros3/logout.pl
+++ ficheros3/logout.pl
@@ -8,16 +8,25 @@ use CGI::Session;
 
 my $cgi = CGI->new;
 
+CGI::Session->name("ECOSESSION");
+
 my $session = CGI::Session->new(
     undef,
     $cgi,
-    { Directory => '/tmp' }
+    { Directory => '/var/lib/ecosalmantica/sessions' }
 );
 
 $session->delete();
-
 $session->flush();
 
+my $cookie = $cgi->cookie(
+    -name    => 'ECOSESSION',
+    -value   => '',
+    -expires => '-1d',
+    -path    => '/'
+);
+
 print $cgi->redirect(
-    "/index.html"
+    -uri    => '/',
+    -cookie => $cookie
 );
